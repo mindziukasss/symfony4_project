5ca8274382351f1347e546387d139a1440ef4ecf
Mindaugas <mindaugas.bernotas@mediapark.com>
2018-12-29 16:51:00 +0200

    Custom Serialization, User Roles

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -100,7 +100,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"post", "put"})
+     * @Groups({"post", "put", "get-admin", "get-owner"})
      * @Assert\NotBlank()
      * @Assert\Email()
      */
@@ -120,6 +120,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="simple_array", length=200)
+     * @Groups({"get-admin", "get-owner"})
      *
      */
     private $roles;
