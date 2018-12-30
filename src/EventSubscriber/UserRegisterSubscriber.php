2f79e62af94d923269b958d756772237526e44d3
Mindaugas <mindaugas.bernotas@mediapark.com>
2018-12-30 16:36:44 +0200

    Added send confirmation to email

--- src/EventSubscriber/UserRegisterSubscriber.php
+++ src/EventSubscriber/UserRegisterSubscriber.php
@@ -1,18 +1,14 @@
 <?php
-
 namespace App\EventSubscriber;
-
-
 use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Email\Mailer;
 use App\Entity\User;
 use App\Security\TokenGenerator;
-use Swift_Message;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-
 class UserRegisterSubscriber implements EventSubscriberInterface
 {
     /**
@@ -22,59 +18,45 @@ class UserRegisterSubscriber implements EventSubscriberInterface
     /**
      * @var TokenGenerator
      */
-    private $generator;
+    private $tokenGenerator;
     /**
-     * @var \Swift_Mailer
+     * @var Mailer
      */
     private $mailer;
-
-    /**
-     * PasswordHashSubscriber constructor.
-     *
-     * @param UserPasswordEncoderInterface $passwordEncoder
-     * @param TokenGenerator               $generator
-     * @param \Swift_Mailer                $mailer
-     */
     public function __construct(
         UserPasswordEncoderInterface $passwordEncoder,
-        TokenGenerator $generator,
-        \Swift_Mailer $mailer
-    ) {
+        TokenGenerator $tokenGenerator,
+        Mailer $mailer
+    )
+    {
         $this->passwordEncoder = $passwordEncoder;
-        $this->generator = $generator;
-
+        $this->tokenGenerator = $tokenGenerator;
         $this->mailer = $mailer;
     }
-
     public static function getSubscribedEvents()
     {
         return [
             KernelEvents::VIEW => ['userRegistered', EventPriorities::PRE_WRITE],
         ];
     }
-
     public function userRegistered(GetResponseForControllerResultEvent $event)
     {
         $user = $event->getControllerResult();
-        $method = $event->getRequest()->getMethod();
-
+        $method = $event->getRequest()
+            ->getMethod();
         if (!$user instanceof User ||
             !in_array($method, [Request::METHOD_POST])) {
             return;
         }
-
+        // It is an User, we need to hash password here
         $user->setPassword(
             $this->passwordEncoder->encodePassword($user, $user->getPassword())
         );
-
-        $user->setConfirmationToken($this->generator->getRandomSecureToken());
-
-
-        $message = (new Swift_Message('Hello From API PLATFORM!'))
-            ->setFrom('mindaugasbernotas2@gmail.com')
-            ->setTo('mindaugasbernotas2@gmail.com')
-            ->setBody('Hello, how are you?');
-        $this->mailer->send($message);
-
+        // Create confirmation token
+        $user->setConfirmationToken(
+            $this->tokenGenerator->getRandomSecureToken()
+        );
+        // Send e-mail here...
+        $this->mailer->sendConfirmationEmail($user);
     }
 }
